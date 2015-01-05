99e37dccb854177de7b7f610920f320bf526c69a
Ivo Ugrina <ivo@iugrina.com>
2015-01-05 15:04:32 +0100

    kozmetika

--- dezurstva/statistika/views.py
+++ dezurstva/statistika/views.py
@@ -4,43 +4,41 @@ from dezurstva.osobe.models import *
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
-
 import datetime
+
+# podesi ovo za prikaz ("resetiranje" brojaca)
+POCETAK = datetime.datetime(2012,9,1)
 
 ZADNJIH_X_DANA = [30,90,180,360]
 
 LOG_LENGTH = 50
 
+
 class OsobaStatistika():
     def __init__(self, osoba):
         self.osoba = osoba
-    
+
     def sati(self, zadnjihD = [] ):
 
-        def f( i, d, pocetak ) :
-            if( d < i.datum and i.datum > pocetak ): return int(i.sati)
+        def f( i, d, POCETAK) :
+            if( d < i.datum and i.datum > POCETAK): return int(i.sati)
             return 0
 
         cuvanjaOsobe = self.osoba.cuvanje_set.all()
 
-        pocetak = datetime.datetime(2012,9,1)
-
         if zadnjihD == [] :
             ukupno = 0
             for i in cuvanjaOsobe :
-                if i.datum > pocetak :
+                if i.datum > POCETAK:
                     ukupno += i.sati
             return ukupno
         else :
             satiD = map( lambda x: 0, zadnjihD )
-            datetimeD = [ datetime.datetime.now() - datetime.timedelta(days=d) for d in zadnjihD ] 
+            datetimeD = [ datetime.datetime.now() - datetime.timedelta(days=d) for d in zadnjihD ]
             for i in cuvanjaOsobe :
-                tmp = [ f(i,d,pocetak) for d in datetimeD ]
+                tmp = [ f(i,d,POCETAK) for d in datetimeD ]
                 satiD = [ i+j for i,j in zip( satiD, tmp) ]
             return satiD
-            
-
-
 
 
 def index(request, sort_id=0):
@@ -73,7 +71,7 @@ def index(request, sort_id=0):
 
     t = 'index.html'
 
-    return render_to_response(t, 
+    return render_to_response(t,
         {'stat_list': stat, 'logs': cuvanja, 'sort': ZADNJIH_X_DANA })
 
 
